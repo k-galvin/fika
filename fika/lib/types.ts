deefb1809adc47a50528b5902c038cc9b5c27f88
ahtzirigutierrez <ahtzirigutierrez1102@gmail.com>
2025-10-31 10:57:17 -0700

    fix: build error

--- fika/lib/types.ts
+++ fika/lib/types.ts
@@ -1,12 +1,20 @@
 import { Database } from "./supabase/database.types";
 
-export type SuggestedCafe = Database["public"]["Tables"]["suggested_cafes"]["Row"];
-export type UserSavedCafe = Database["public"]["Tables"]["user_saved_cafes"]["Row"] & {
-  coffee_shops: CoffeeShop;
+export type CoffeeShop = Database["public"]["Tables"]["coffee_shops"]["Row"] & {
+  shop_photos: { photo_url: string }[];
+  isInitiallySaved: boolean;
+  isInitiallyVisited: boolean;
+  ratings: { user_id: string; drinks_quality: number | null }[];
 };
+
+export type SuggestedCafe =
+  Database["public"]["Tables"]["suggested_cafes"]["Row"];
+
+// New types for user_visits and user_saved_cafes
 export type UserVisit = Database["public"]["Tables"]["user_visits"]["Row"] & {
-  coffee_shops: CoffeeShop;
+  coffee_shops: CoffeeShop | null;
 };
-
-export type CoffeeShop = Database["public"]["Tables"]["coffee_shops"]["Row"];
-
+export type UserSavedCafe =
+  Database["public"]["Tables"]["user_saved_cafes"]["Row"] & {
+    coffee_shops: CoffeeShop | null;
+  };
